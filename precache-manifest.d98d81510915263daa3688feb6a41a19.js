1c6f3f2c3493c7e028e68b1d5abac96342aa7e5c
ashish <avsavani@gmail.com>
2020-03-04 20:57:59 -0800

    Updates

---
+++ precache-manifest.d98d81510915263daa3688feb6a41a19.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26b6c8cca6026663392535a671069b3",
+    "url": "/watershed-guardians/index.html"
+  },
+  {
+    "revision": "d9b01fcadcd00fd411f3",
+    "url": "/watershed-guardians/static/css/2.2614ebd1.chunk.css"
+  },
+  {
+    "revision": "18f37c1dca4787fadd43",
+    "url": "/watershed-guardians/static/css/main.55ca001a.chunk.css"
+  },
+  {
+    "revision": "d9b01fcadcd00fd411f3",
+    "url": "/watershed-guardians/static/js/2.9634bf5a.chunk.js"
+  },
+  {
+    "revision": "18f37c1dca4787fadd43",
+    "url": "/watershed-guardians/static/js/main.70cbe11f.chunk.js"
+  },
+  {
+    "revision": "acbc88cdb640a408ab2b",
+    "url": "/watershed-guardians/static/js/runtime~main.226dcd1f.js"
+  },
+  {
+    "revision": "40782821a554d83e0a05bfceed306793",
+    "url": "/watershed-guardians/static/media/about-page.40782821.jpg"
+  },
+  {
+    "revision": "0bb5d45da9f161f69c14470546f5c746",
+    "url": "/watershed-guardians/static/media/monarch.0bb5d45d.png"
+  },
+  {
+    "revision": "0324da6236bc08780851d25a3c9ecbe5",
+    "url": "/watershed-guardians/static/media/tests.0324da62.png"
+  }
+]);
